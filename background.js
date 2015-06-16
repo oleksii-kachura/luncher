7ed2b333914150efb0ba4c500065847265c5ae76
alex-kachura <alex.kachura@gmail.com>
2015-06-16 22:29:19 +0200

    proper browser tabs fix

--- background.js
+++ background.js
@@ -1,8 +1,12 @@
 /**
- * Shows extension icon in the address bar.
+ * Shows extension icon in the address bar for browser tabs with Maxymiser UI.
  * @param tabId - ID of the current tab.
+ * @param changeInfo
+ * @param tab - Current tab.
  */
-function showExtensionIcon(tabId) {
-    chrome.pageAction.show(tabId);
+function checkForValidUrl(tabId, changeInfo, tab) {
+    if (/(ui61.*|demo).maxymiser.(com|org)/.test(tab.url)) {
+        chrome.pageAction.show(tabId);
+    }
 }
-chrome.tabs.onUpdated.addListener(showExtensionIcon);
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
